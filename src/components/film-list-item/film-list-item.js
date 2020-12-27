3e215ee42ccebe962e05869a3ff7987ddb3237b1
Andrey <brenar123@gmail.com>
2020-12-27 19:00:58 +0300

    add textArea

--- src/components/film-list-item/film-list-item.js
+++ src/components/film-list-item/film-list-item.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+import './film-list-item.css';
 
 const FilmListItem = ({filmData, handleCheckLine}) => {
+
+    //const [name, setName] = useState('')
+
   const {id, name, description, genre, rating} = filmData
     return (
         <tr>
             <td>{id}</td>
-            <td><textarea>{name}</textarea></td>
+            <td><textarea className="area">{name}</textarea></td>
             <td><textarea>{description}</textarea></td>
-            <td>{genre}</td>
-            <td>{rating}</td>
+            <td><textarea>{genre}</textarea></td>
+            <td><textarea className="rating-area">{rating}</textarea></td>
             <td><input type="checkbox" onClick={() => handleCheckLine(id)}/></td>
         </tr>
     )
