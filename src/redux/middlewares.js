1103cf3b7e928432e964d994a06e61665b58e953
Andrey <brenar123@gmail.com>
2021-01-26 14:50:58 +0300

    middleware

--- src/redux/middlewares.js
+++ src/redux/middlewares.js
@@ -1,18 +1,18 @@
 import {setItemToLocalStorage} from '../utils'
+//import validate from '../components/AddedForm/validate'
 
 import {ADD_FILM, HANLDE_ASCENDING, HANDLE_DISCENDING, HANDLE_DELETE_FILM, HANDLE_SAVE_CHANGES} from '../models/currency'
 const persistActionsArray = [ADD_FILM, HANLDE_ASCENDING, HANDLE_DISCENDING, HANDLE_DELETE_FILM, HANDLE_SAVE_CHANGES]
+// export const logger = ({dispatch, getState}) => next => (actionCreator) => {
 
-export const logger = ({dispatch, getState}) => next => (actionCreator) => {
+//   console.log('prevState$$$$$$$$$$$$$$', getState())
+//   console.log('----------------->')
+//   console.log(actionCreator.type, '^^^^^^^^^^', actionCreator.payload)
+//   console.log('----------------->')
+//   console.log(actionCreator.payload)
 
-  console.log('prevState$$$$$$$$$$$$$$', getState())
-  console.log('----------------->')
-  console.log(actionCreator.type, '^^^^^^^^^^', actionCreator.payload)
-  console.log('----------------->')
-  console.log(actionCreator.payload)
-
-  return next(actionCreator)
-}
+//   return next(actionCreator)
+// }
 
 
 
@@ -23,6 +23,15 @@ export const persistDataToLocalstorage = () => next => ({type, payload}) => {
     } else {
       setItemToLocalStorage('filmList', payload.filmData)
     }
+  }
+  return next({type, payload})
+}
+
+export const validateData = () => next => ({type, payload}) => {
+  if(type == ADD_FILM){
+    //if(payload.name == undefined) {
+      console.log('error')
+    //}
   }
   return next({type, payload})
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -4,9 +4,9 @@ import thunk from 'redux-thunk'
 import reducer from './reducer'
 import { routerMiddleware } from 'connected-react-router'
 import { history } from '../history'
-import {persistDataToLocalstorage} from './middlewares'
+import {persistDataToLocalstorage, validateData} from './middlewares'
 
-const enhancer = applyMiddleware(thunk, routerMiddleware(history), logger, persistDataToLocalstorage)
+const enhancer = applyMiddleware(thunk, routerMiddleware(history), logger, persistDataToLocalstorage, validateData)
 const store = createStore(reducer, enhancer)
 
 export default store
